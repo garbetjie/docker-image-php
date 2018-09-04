82e37555a9d916ca89631a73c7e5d1f4f24e5ae9
Geoff Garbers <geoff@garbers.co.za>
2018-09-04 07:02:17 +0200

    Remove .tpl extension for FPM configuration file.

--- fs/etc/cont-init.d/20-write-fpm-config.sh
+++ fs/etc/cont-init.d/20-write-fpm-config.sh
@@ -2,13 +2,13 @@
 set -e
 
 # If mo max children is defined, then calculate the max number of children allowed as 80% of available RAM, assuming
-# ${PHP_MEMORY_LIMIT}MB per process.
-[[ $FPM_MAX_CHILDREN -lt 1 ]] && export FPM_MAX_CHILDREN=`expr "$(grep MemTotal /proc/meminfo | awk '{print $2}')" "*" 80 "/" 100 "/" 1024 "/" $(echo "$PHP_MEMORY_LIMIT" | grep -oE '[0-9]+')`
+# ${MEMORY_LIMIT}MB per process.
+[[ $FPM_MAX_CHILDREN -lt 1 ]] && export FPM_MAX_CHILDREN=`expr "$(grep MemTotal /proc/meminfo | awk '{print $2}')" "*" 80 "/" 100 "/" 1024 "/" $(echo "$MEMORY_LIMIT" | grep -oE '[0-9]+')`
 
 # Update php-fpm.conf
-envsubst '
-$FPM_PM
-$FPM_MAX_CHILDREN
-$FPM_MIN_SPARE_SERVERS
-$FPM_MAX_SPARE_SERVERS
-$FPM_MAX_REQUESTS' < $PHP_INI_DIR/php-fpm.conf.tpl > $PHP_INI_DIR/php-fpm.conf
+envsubst '$FPM_PM
+          $FPM_MAX_CHILDREN
+          $FPM_MIN_SPARE_SERVERS
+          $FPM_MAX_SPARE_SERVERS
+          $FPM_MAX_REQUESTS
+          $TIMEOUT' < $PHP_INI_DIR/php-fpm.conf > $PHP_INI_DIR/php-fpm.conf
