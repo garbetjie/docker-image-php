abfd420a91085737b40e06871b717ef11551adbf
Geoff Garbers <geoff@garbers.co.za>
2018-09-04 21:43:35 +0200

    Remove -newrelic tag.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,5 +6,4 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Push images.
 docker push garbetjie/php:${VERSION}-onbuild
-docker push garbetjie/php:${VERSION}-newrelic
 docker push garbetjie/php:${VERSION}
