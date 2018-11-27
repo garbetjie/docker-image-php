e87889ae84d4350b6162cffbd26d76408edeb5b8
Geoff Garbers <geoff@garbers.co.za>
2018-11-27 10:25:08 +0200

    Add commit-based tagging.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,8 +2,11 @@
 set -ex
 
 # Log into Docker registry.
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_LOGIN" --password-stdin
 
 # Push images.
-docker push garbetjie/php:${VERSION}-onbuild
-docker push garbetjie/php:${VERSION}
+docker push ${DOCKER_HUB_REPO}/php:${VERSION}-onbuild
+docker push ${DOCKER_HUB_REPO}/php:${VERSION}-onbuild-${SHORT_COMMIT}
+
+docker push ${DOCKER_HUB_REPO}/php:${VERSION}
+docker push ${DOCKER_HUB_REPO}/php:${VERSION}-${SHORT_COMMIT}
